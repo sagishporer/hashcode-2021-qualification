672d0a7921dea6247ea6ad3fb1d2271ac1b3f50b
Sagi <shporer@gmail.com>
2021-03-03 11:25:46 +0200

    Added helper methods

--- hashcode2021/Utils.cs
+++ hashcode2021/Utils.cs
@@ -29,5 +29,61 @@ namespace hashcode2021
                 index = ~index;
             list.Insert(index, item);
         }
+
+        public static void SwapItems<T>(List<T> list1, int item1, List<T> list2, int item2)
+        {
+            T tmp = list1[item1];
+            list1[item1] = list2[item2];
+            list2[item2] = tmp;
+        }
+
+        public static int IntersectSize(int[] a, int[] b)
+        {
+            int size = 0;
+            int posA = 0;
+            int posB = 0;
+
+            while ((posA < a.Length) && (posB < b.Length))
+            {
+                if (a[posA] < b[posB])
+                    posA++;
+                else if (a[posA] > b[posB])
+                    posB++;
+                else if (a[posA] == b[posB])
+                {
+                    size++;
+                    posA++;
+                    posB++;
+                }
+                else
+                    throw new Exception("Ha?");
+            }
+
+            return size;
+        }
+
+        public static int IntersectSize<T>(HashSet<T> a, HashSet<T> b)
+        {
+            HashSet<T> small;
+            HashSet<T> big;
+            if (a.Count < b.Count)
+            {
+                small = a;
+                big = b;
+            }
+            else
+            {
+                small = b;
+                big = a;
+            }
+
+            int size = 0;
+            // Iterate on the smaller HashSet (faster)
+            foreach (T val in small)
+                if (big.Contains(val))
+                    size++;
+
+            return size;
+        }
     }
 }
