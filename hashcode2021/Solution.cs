e4b37b08f23532c839a775b2d1309128e6abc741
Sagi <shporer@gmail.com>
2021-03-09 14:37:59 +0200

    Add method: Load solution from file

--- hashcode2021/Solution.cs
+++ hashcode2021/Solution.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace hashcode2021
@@ -19,6 +21,49 @@ namespace hashcode2021
             this.Intersections = new SolutionIntersection[other.Intersections.Length];
             for (int i = 0; i < this.Intersections.Length; i++)
                 this.Intersections[i] = (SolutionIntersection)other.Intersections[i].Clone();
+        }
+
+        public static Solution LoadSolution(string fileName, Problem problem)
+        {
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                int maxIntersection = -1;
+                int numberOfIntersectionsWithGreenLight = int.Parse(sr.ReadLine());
+                Dictionary<int, SolutionIntersection> intersections = new Dictionary<int, SolutionIntersection>();
+                for (int i = 0; i < numberOfIntersectionsWithGreenLight; i++)
+                {
+                    int intersectionId = int.Parse(sr.ReadLine());
+                    maxIntersection = Math.Max(intersectionId, maxIntersection);
+
+                    int numberOfStreets = int.Parse(sr.ReadLine());
+                    SolutionIntersection solutionIntersection = new SolutionIntersection(intersectionId);
+                    solutionIntersection.GreenLigths = new List<GreenLightCycle>();
+                    for (int s = 0; s < numberOfStreets; s++)
+                    {
+                        string[] parts = sr.ReadLine().Split(" ");
+                        string streetName = parts[0];
+                        int duration = int.Parse(parts[1]);
+                        Street street = problem.Streets[streetName];
+
+                        GreenLightCycle greenLightCycle = new GreenLightCycle(street);
+                        greenLightCycle.Duration = duration;
+                        solutionIntersection.GreenLigths.Add(greenLightCycle);
+                    }
+
+                    intersections.Add(intersectionId, solutionIntersection);
+                }
+
+                Solution solution = new Solution(maxIntersection + 1);
+                for (int i = 0; i < maxIntersection + 1; i++)
+                {
+                    if (intersections.ContainsKey(i))
+                        solution.Intersections[i] = intersections[i];
+                    else
+                        solution.Intersections[i].GreenLigths = new List<GreenLightCycle>();
+                }
+
+                return solution;
+            }
         }
 
         public int CountIntersectionsWithGreenLights()
